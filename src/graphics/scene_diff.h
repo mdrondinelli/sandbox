ed1a27130d3dd4bb38f3d69677af5a04304a12ed
Marlon Rondinelli <mdrondinelli@gmail.com>
2023-07-24 11:44:53 -0700

    Remove wrong and unused includes

--- src/graphics/scene_diff.h
+++ src/graphics/scene_diff.h
@@ -1,9 +1,6 @@
 #ifndef MARLON_GRAPHICS_SCENE_DIFF_H
 #define MARLON_GRAPHICS_SCENE_DIFF_H
 
-#include "../../math/quat.h"
-#include "../../math/vec.h"
-
 namespace marlon {
 namespace graphics {
 class Scene;
