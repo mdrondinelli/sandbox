2d58e82864a7063f169ffac5a6de4c8be155aa0b
Marlon Rondinelli <mdrondinelli@gmail.com>
2023-09-22 22:00:14 -0700

    Update main physics header

--- src/physics/physics.h
+++ src/physics/physics.h
@@ -1,7 +1,9 @@
 #ifndef MARLON_PHYSICS_PHYSICS_H
 #define MARLON_PHYSICS_PHYSICS_H
 
+#include "aabb_tree.h"
 #include "aabb.h"
+#include "dynamic_rigid_body.h"
 #include "material.h"
 #include "particle.h"
 #include "shape.h"
