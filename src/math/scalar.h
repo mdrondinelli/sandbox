483cc1ba301d03673a3345dabbd2798fbe026d30
Marlon Rondinelli <mdrondinelli@gmail.com>
2024-05-15 13:32:22 -0700

    Add isinf

--- src/math/scalar.h
+++ src/math/scalar.h
@@ -3,38 +3,35 @@
 
 #include <cmath>
 
+#include <concepts>
+
 namespace marlon {
-  namespace math {
-    template <typename T>
-    constexpr T const &min(T const &a, T const &b) noexcept {
-      return a < b ? a : b;
-    }
-
-    template <typename T>
-    constexpr T const &max(T const &a, T const &b) noexcept {
-      return a < b ? b : a;
-    }
-
-    template <typename T>
-    constexpr T const &clamp(T const &v, T const &lo, T const &hi) noexcept {
-      return min(max(v, lo), hi);
-    }
-
-    template <typename T>
-    constexpr T abs(T x) noexcept {
-      return std::abs(x);
-    }
-
-    template <typename T>
-    constexpr T pow(T x, T y) noexcept {
-      return std::pow(x, y);
-    }
-
-    template <typename T>
-    constexpr T sqrt(T x) noexcept {
-      return std::sqrt(x);
-    }
-  }
+namespace math {
+template <typename T> constexpr T const &min(T const &a, T const &b) noexcept {
+  return a < b ? a : b;
 }
+
+template <typename T> constexpr T const &max(T const &a, T const &b) noexcept {
+  return a < b ? b : a;
+}
+
+template <typename T>
+constexpr T const &clamp(T const &v, T const &lo, T const &hi) noexcept {
+  return min(max(v, lo), hi);
+}
+
+template <typename T> constexpr T abs(T x) noexcept { return std::abs(x); }
+
+template <typename T> constexpr T pow(T x, T y) noexcept {
+  return std::pow(x, y);
+}
+
+template <typename T> constexpr T sqrt(T x) noexcept { return std::sqrt(x); }
+
+template <std::floating_point T> bool isinf(T x) noexcept {
+  return std::isinf(x);
+}
+} // namespace math
+} // namespace marlon
 
 #endif
