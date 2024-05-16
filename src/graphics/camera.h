a177e29dcd625bac7a0203b7398ca63dcc1cdd44
Marlon Rondinelli <mdrondinelli@gmail.com>
2024-05-15 20:32:56 -0700

    Add members to Camera for cascaded shadow maps

--- src/graphics/camera.h
+++ src/graphics/camera.h
@@ -10,7 +10,10 @@ struct Camera {
   math::Quatf orientation{math::Quatf::identity()};
   math::Vec2f zoom{math::Vec2f::all(1.0f)};
   float near_plane_distance{0.1f};
-  float exposure{0.01f};
+  float cascaded_shadow_map_distance{1000.0f};
+  int cascaded_shadow_map_slices{4};
+  int cascaded_shadow_map_resolution{1024};
+  float exposure{0.02f};
 };
 } // namespace graphics
 } // namespace marlon
