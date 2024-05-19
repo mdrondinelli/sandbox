865d3edcb854cff163abd94738b13363ddbc9a0c
Marlon Rondinelli <mdrondinelli@gmail.com>
2024-05-18 20:46:07 -0700

    Tweak defaults

--- src/graphics/camera.h
+++ src/graphics/camera.h
@@ -10,9 +10,9 @@ struct Camera {
   math::Quatf orientation{math::Quatf::identity()};
   math::Vec2f zoom{math::Vec2f::all(1.0f)};
   float near_plane_distance{0.1f};
-  int csm_texture_resolution{2048};
-  int csm_cascade_count{5};
-  float csm_render_distance{100.0f};
+  int csm_texture_resolution{4096};
+  int csm_cascade_count{3};
+  float csm_render_distance{200.0f};
   float exposure{0.03f};
 };
 } // namespace graphics
