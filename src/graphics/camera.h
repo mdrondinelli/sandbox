31d1d0fd9e8e74d0159ff30d84135a01b8a28207
Marlon Rondinelli <mdrondinelli@gmail.com>
2024-05-20 02:57:45 -0700

    reduce cascade count

--- src/graphics/camera.h
+++ src/graphics/camera.h
@@ -11,7 +11,7 @@ struct Camera {
   math::Vec2f zoom{math::Vec2f::all(1.0f)};
   float near_plane_distance{0.1f};
   int csm_texture_resolution{4096};
-  int csm_cascade_count{4};
+  int csm_cascade_count{3};
   float csm_render_distance{200.0f};
   float exposure{0.03f};
 };
