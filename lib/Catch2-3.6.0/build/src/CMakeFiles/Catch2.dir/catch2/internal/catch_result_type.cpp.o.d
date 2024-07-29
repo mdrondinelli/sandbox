a4c458c28bcac54b49f2091b5433f74cfd1da620
Marlon Rondinelli <marlonrondinelli@pop-os.attlocal.net>
2024-07-29 00:54:53 -0700

    Add Catch2 source code

---
+++ lib/Catch2-3.6.0/build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_result_type.cpp.o.d
@@ -0,0 +1,4 @@
+src/CMakeFiles/Catch2.dir/catch2/internal/catch_result_type.cpp.o: \
+ /home/marlonrondinelli/Catch2-3.6.0/src/catch2/internal/catch_result_type.cpp \
+ /usr/include/stdc-predef.h \
+ /home/marlonrondinelli/Catch2-3.6.0/src/catch2/../catch2/internal/catch_result_type.hpp
